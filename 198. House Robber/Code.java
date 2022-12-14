e1985209fe213cc0183096063f9961b3e2e96988
prabal262002 <86048405+prabal262002@users.noreply.github.com>
2022-12-15 02:34:45 +0530

    Runtime 0 ms Beats 100% Memory 39.6 MB Beats 90.90%

---
+++ 198. House Robber/Code.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n==1) return nums[0];
+        int [] arr = new int[n];
+        arr[0] =nums[0];
+        arr[1] = Math.max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+          arr[i] = Math.max(arr[i-2]+nums[i],arr[i-1]);
+        }
+        return arr[n-1];
+    }
+}
